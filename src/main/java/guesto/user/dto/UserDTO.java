574ffdbfc32d84f2a51a510369f095a650f3fcaf
Leo Bowen Wang <leobwang@gmail.com>
2024-01-15 17:56:31 +0100

    add serdable to UserDTO

--- src/main/java/guesto/user/dto/UserDTO.java
+++ src/main/java/guesto/user/dto/UserDTO.java
@@ -1,8 +1,10 @@
 package guesto.user.dto;
 
 import io.micronaut.core.annotation.Introspected;
+import io.micronaut.serde.annotation.Serdeable;
 
 @Introspected
+@Serdeable
 public class UserDTO {
 
     private Long id;
