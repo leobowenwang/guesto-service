006e736d448f4c09a2c2161341304df158b1726c
goldis <goldis.salarmaaf@hotmail.com>
2024-01-15 16:15:27 +0100

    vuetify added, registrationform and loginform finished

--- src/main/webapp/gusto-app/src/views/LoginForm.vue
+++ src/main/webapp/gusto-app/src/views/LoginForm.vue
@@ -1,22 +1,51 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to LoginForm"/>
+  <v-alert v-if="loginSuccess" type="success">Login erfolgreich!</v-alert>
+  <v-alert v-if="loginFailed" type="error">Login fehlgeschlagen!</v-alert>
+  <div>
+    <h2>Login</h2>
+    <v-form @submit.prevent>
+      <v-text-field type="text" id="username" v-model="formData.username" required label="Benutzername"></v-text-field>
+      <v-text-field type="password" id="password" v-model="formData.password" required label="Passwort"></v-text-field>
+      <v-btn class="text-none mb-4" color="#48EDDD" size="x-large" block @click="submitForm()">Anmelden</v-btn>
+    </v-form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+const BASE_URL='http://localhost:8080/user/login';
 
 export default {
-  name: 'LoginForm',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      formData: {
+        username: '',
+        password: ''
+      },
+      loginSuccess: false,
+      loginFailed: false
+    };
   },
   methods: {
-    loginUser() {
-      console.log("login!");
+    async submitForm() {
+      this.loginSuccess = false;
+      this.loginFailed = false;
+      try {
+        console.log(this.formData);
+        const response = await this.$axios.post(BASE_URL, this.formData);
+        this.loginSuccess = true;
+        console.log('Anmeldung erfolgreich:', response.data);
+        setTimeout(() => {
+          this.$router.push('/events');
+        }, 1000);
+      } catch (error) {
+        this.loginFailed = true;
+        console.error('Fehler bei der Anmeldung:', error);
+      }
     }
-  }
+  },
+  created() {
+  },
+  name: 'LoginForm'
 }
 </script>
