0f08e60300284beffa673a79e30aeae0018bd864
goldis <goldis.salarmaaf@hotmail.com>
2024-01-16 15:22:22 +0100

    event detail delete

--- src/main/webapp/gusto-app/src/auth/auth-header.js
+++ src/main/webapp/gusto-app/src/auth/auth-header.js
@@ -3,7 +3,6 @@ export default function authHeader() {
     let access_token = store.state.auth.access_token
   
     if (access_token) {
-      console.log(access_token);
       return { Authorization: 'Bearer ' + access_token };
     } else {
       return {};
