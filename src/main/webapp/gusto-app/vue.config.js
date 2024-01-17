f2d579a03c534c07fb794fde84f2b36caf0d6d1c
Mehmet Arslan <82936664+MehmetArslan01@users.noreply.github.com>
2024-01-17 13:19:02 +0100

    Vue configs

--- src/main/webapp/gusto-app/vue.config.js
+++ src/main/webapp/gusto-app/vue.config.js
@@ -1,7 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/guesto-service/' : '/',
   devServer: {
     port: 8081
   },
   transpileDependencies: true
-})
+});
